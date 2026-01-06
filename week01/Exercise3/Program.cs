8ba08032f72d9f0fb0206d8b05ba10543b2e7c80
Your Name <your@email.com>
2026-01-06 08:38:05 -0500

    program

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -9,10 +9,14 @@ class Program
         int magicNumber = randomGenerator.Next(1,21);
 
         int guess = 0;
+        int guessCount = 0;
         while (guess != magicNumber)
+        
         {
             Console.Write("Please enter a guess number between 1 and 20: ");
             guess = int.Parse(Console.ReadLine());
+
+            guessCount++;
 
             if (guess > magicNumber)
             {
@@ -25,7 +29,15 @@ class Program
             else
             {
                 Console.WriteLine("You guessed it!");
+                Console.WriteLine($"You have guessed {guessCount} times.");
             }
+
+            Console.WriteLine("Would you like to play again? (y/n): ");
+            string playAgain = Console.ReadLine();
+
+            Console.WriteLine("Thanks for playing!!");
+
+
         }
 
     }
