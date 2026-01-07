bcf50bc4e157153355ac7cefda7c78d8bbbe3cdf
Your Name <your@email.com>
2026-01-06 22:27:03 -0500

    program

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -5,23 +5,21 @@ using System.Globalization;
 class Program
 {
     static void Main(string[] args)
-
     {
         Console.WriteLine("Enter a list of numbers, type 0 when finished: ");
 
         int number = -1;
         int sum = 0;
-   
-}
 
-    while (number != 0)
-    {
-        number = int.Parse(Console.ReadLine());
-        if (number != 0)
+        while (number != 0)
         {
-            sum += number;
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                sum += number;
+            }
         }
-    }
 
-    Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The sum is: {sum}");
+    }
 }
