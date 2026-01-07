dfca5c98298705d65448cff80755028ca8ac669d
Your Name <your@email.com>
2026-01-06 22:18:25 -0500

    project

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished: ");
+
+        int number = -1;
+        int sum = 0;
+   
+}
+
+    while (number != 0)
+    {
+        number = int.Parse(Console.ReadLine());
+        if (number != 0)
+        {
+            sum += number;
+        }
     }
+
+    Console.WriteLine($"The sum is: {sum}");
 }
