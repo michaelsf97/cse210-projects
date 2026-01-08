b6f1ca6b4506510c0512c076efecd79c5fc4f5d9
Your Name <your@email.com>
2026-01-07 22:04:28 -0500

    program

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
-    }
+        Console.WriteLine("Welcome to the program!");
+
+        {
+            Console.WriteLine("Please enter your name:");
+        }
+
+}
 }
