d1235e7806acf8bc25e026647bfd5d5d6b881678
Your Name <your@email.com>
2026-01-07 23:09:22 -0500

    program

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,14 +1,33 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to the program!");
+        string name = PromptUserName();
+        int number = PromptFavoriteNumber();
+        int square = SquareNumber();
+        Console.WriteLine($"{name}, the square of your favorite number is: {square}");
+    }
 
-        {
-            Console.WriteLine("Please enter your name:");
-        }
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        return Console.ReadLine();
+    }
 
-}
+    static int PromptFavoriteNumber()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        string input = Console.ReadLine();
+        int number; int.Parse(input);
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
 }
