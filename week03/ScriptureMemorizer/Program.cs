304eeef2002d5f2a2d9cefbaae7a649d3b933529
Your Name <your@email.com>
2026-01-27 18:35:01 -0500

    program

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -3,7 +3,39 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Console.WriteLine("Welcome to the Scripture Memorizer");
+
+        
+        List<string> scriptures = new List<string>();
+        scriptures.Add("John 3:16 ¶ For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
+        scriptures.Add("Proverbs 3:5-6 5 ¶ Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.");
+        scriptures.Add("Philippians 4:13 ¶ I can do all things through Christ which strengtheneth me.");
+        scriptures.Add("Psalm 23:1 ¶ The Lord is my shepherd; I shall not want.");
+        scriptures.Add("Romans 8:28 ¶ And we know that all things work together for good to them that love God, to them who are the called according to his purpose.");
+        scriptures.Add("Isaiah 41:10 ¶ Fear thou not; for I am with thee: be not dismayed; for I am thy God: I will strengthen thee; yea, I will help thee; yea, I will uphold thee with the right hand of my righteousness.");
+        scriptures.Add("Matthew 11:28 ¶ Come unto me, all ye that labour and are heavy laden, and I will give you rest.");
+        scriptures.Add("2 Timothy 1:7 ¶ For God hath not given us the spirit of fear; but of power, and of love, and of a sound mind.");
+        scriptures.Add("Hebrews 11:1 ¶ Now faith is the substance of things hoped for, the evidence of things not seen.");
+        scriptures.Add("Jeremiah 29:11 ¶ For I know the thoughts that I think toward you, saith the Lord, thoughts of peace, and not of evil, to give you an expected end.");
+
+        foreach (string scripture in scriptures)
+        {
+            Console.WriteLine(scripture);
+                    Console.WriteLine("Press Enter to continue or type 'quit' to exit.");
+                    string input = Console.ReadLine();
+                    if (input.ToLower() == "quit")
+            {
+                break;
+            }
+            
+        }
+
+        Console.Clear();
+
+        {
+            
+        }
     }
 }
