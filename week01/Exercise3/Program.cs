ca37dd47727d3999545c0454ecdb36d025892f74
Your Name <your@email.com>
2026-01-27 12:05:43 -0500

    program

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,42 +3,47 @@ using System;
 class Program
 {
     static void Main(string[] args)
-
     {
         Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1,21);
-
+        int magicNumber = randomGenerator.Next(1, 21); // random number between 1 and 20
         int guess = 0;
         int guessCount = 0;
+
+        Console.WriteLine("Guess the magic number between 1 and 20!");
         while (guess != magicNumber)
-        
         {
-            Console.Write("Please enter a guess number between 1 and 20: ");
-            guess = int.Parse(Console.ReadLine());
+            Console.Write("What is your guess? ");
+            string input = Console.ReadLine();
 
-            guessCount++;
-
-            if (guess > magicNumber)
+            if (input.ToLower() == "quit")
             {
-                Console.WriteLine("Lower");
+                Console.WriteLine("You quit the game.");
+                return;
             }
-            else if (guess < magicNumber)
+
+            if (int.TryParse(input, out guess))
             {
-                Console.WriteLine("Higher");
+                guessCount++;
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"You have guessed {guessCount} times.");
+                }
             }
             else
             {
-                Console.WriteLine("You guessed it!");
-                Console.WriteLine($"You have guessed {guessCount} times.");
+                Console.WriteLine("Please enter a valid number or type 'quit' to exit.");
             }
-
-            Console.WriteLine("Would you like to play again? (y/n): ");
-            string playAgain = Console.ReadLine();
-
-            Console.WriteLine("Thanks for playing!!");
-
-
         }
 
+        Console.WriteLine("Thanks for playing!!");
     }
 }
