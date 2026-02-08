8652f2c8fafdebe2d93d64e3074e0493924abe41
Your Name <your@email.com>
2026-02-08 18:53:15 -0500

    button

---
+++ week04/OnlineOrdering/Button.cs
@@ -0,0 +1,40 @@
+using System;
+
+public class Button
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public void Click()
+    {
+        Console.WriteLine("Button clicked!");
+    }
+}
+
+public class TextButton : Button
+{
+    public string Text { get; set; }
+}
+
+public class ImageButton : Button
+{
+    public string ImagePath { get; set; }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        TextButton tb = new TextButton();
+        tb.Width = 100;
+        tb.Height = 30;
+        tb.Text = "Click Me!";
+        tb.Click();
+
+        ImageButton ib = new ImageButton();
+        ib.Width = 100;
+        ib.Height = 30;
+        ib.ImagePath = "iconbutton.png";
+        ib.Click();
+    }
+}
