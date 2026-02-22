0853dce3185805b2fb2c73d991a7fe3939f76e56
Your Name <your@email.com>
2026-02-22 15:11:20 -0500

    Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+partial class Employee
+{
+    private float salary; 
+    
+    public virtual float CalculatePay()
+    {
+        return salary;
+
+    }
+
+    public class HourlyEmployee : Employee
+    {
+        public float rate = 9f;
+        public float hours = 100f;
+
+        public override float CalculatePay()
+        {
+            return rate * hours;
+        }
+    }
+
+}
